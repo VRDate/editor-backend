283966b5104bf4afa24df1c7602b9e0ea0230b67
Maxime Gréau <greaumaxime@gmail.com>
2015-03-19 08:26:30 +0100

    handle multiple converter

--- src/main/java/org/asciidoctor/editor/api/ProjectEndpoint.java
+++ src/main/java/org/asciidoctor/editor/api/ProjectEndpoint.java
@@ -1,6 +1,7 @@
 package org.asciidoctor.editor.api;
 
 import org.asciidoctor.editor.datas.ProjectDatas;
+import org.asciidoctor.editor.processor.Converter;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
@@ -16,12 +17,26 @@ public class ProjectEndpoint {
     ProjectDatas datas;
 
     @GET
-	@Path("/{projectId}/file/{fileId}/listener")
-	@Produces(MediaType.APPLICATION_JSON)
+    @Path("/{projectId}/file/{fileId}/listener/{backend}")
+    @Produces(MediaType.APPLICATION_JSON)
 	@Consumes(MediaType.TEXT_PLAIN)
 	public Response listenToAsciiDoc(@PathParam("projectId") String projectId,
-                                 @PathParam("fileId") String fileId) {
-       datas.listenToAsciiDocContent(projectId, fileId);
+                                     @PathParam("fileId") String fileId, @PathParam("backend") String backend) {
+        switch (backend) {
+            case "pdf":
+                datas.listenToAsciiDocContent(projectId, fileId, Converter.pdf);
+                break;
+            case "slide":
+                datas.listenToAsciiDocContent(projectId, fileId, Converter.slide);
+                break;
+            case "html5":
+                datas.listenToAsciiDocContent(projectId, fileId, Converter.html5);
+                break;
+            default:
+                datas.listenToAsciiDocContent(projectId, fileId, Converter.html5);
+        }
+
+
        return Response.ok().build();
 	}
 	
