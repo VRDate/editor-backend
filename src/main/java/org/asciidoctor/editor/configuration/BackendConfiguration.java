bcfa47d20e695e1adfe628b453cabb441b3b0f0d
Maxime Gréau <greaumaxime@gmail.com>
2015-03-23 14:15:29 +0100

    add keys for slides converter

--- src/main/java/org/asciidoctor/editor/configuration/BackendConfiguration.java
+++ src/main/java/org/asciidoctor/editor/configuration/BackendConfiguration.java
@@ -10,6 +10,15 @@ public class BackendConfiguration implements PropertyFileConfig
 
     public static final String KEY_FIREBASE_SECRET = "editor.backend.firebase.secret";
 
+    public static final String ENV_ASCIIDOCTOR_BACKENDS = "ASCIIDOCTOR_BACKENDS";
+
+    public static final String KEY_BACKENDS_DZSLIDES = "editor.backend.backends.dzslides";
+
+    public static final String KEY_BACKENDS_REVEALJS = "editor.backend.backends.revealjs";
+
+    public static final String KEY_BACKENDS_DECKJS = "editor.backend.backends.deckjs";
+
+
     @Override
     public String getPropertyFileName()
     {
